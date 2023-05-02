02e0beadc8675d29c72b0e06982fdbe102a3d97a
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-02 11:37:55 +0600

    chefs section done

--- src/components/ChefData.jsx
+++ src/components/ChefData.jsx
@@ -4,13 +4,18 @@ import SingleChef from "./SingleChef";
 const ChefData = ({ chefData }) => {
   console.log(chefData);
   return (
-    <div className="max-w-7xl mx-auto grid grid-cols-3 gap-10">
-      {chefData?.map((singleChefData) => (
-        <SingleChef
-          key={singleChefData.id}
-          singleChefData={singleChefData}
-        ></SingleChef>
-      ))}
+    <div className="mb-10">
+      <h2 className="text-center text-4xl font-extrabold mb-10">
+        Our Dedicated Chefs!!!
+      </h2>
+      <div className="max-w-7xl mx-auto grid grid-cols-3 gap-10">
+        {chefData?.map((singleChefData) => (
+          <SingleChef
+            key={singleChefData.id}
+            singleChefData={singleChefData}
+          ></SingleChef>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/SingleChef.jsx
+++ src/components/SingleChef.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaBullseye } from "react-icons/fa";
 
 const SingleChef = ({ singleChefData }) => {
   const {
@@ -16,7 +17,20 @@ const SingleChef = ({ singleChefData }) => {
         <img src={chef_picture} alt="" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{chef_name}</h2>
+        <h2 className="card-title text-2xl font-bold">{chef_name}</h2>
+        <p className="flex items-center gap-2 text-base font-medium">
+          <FaBullseye></FaBullseye> Experience : {years_of_experience} years
+        </p>
+        <p className="flex items-center gap-2 text-base font-medium">
+          <FaBullseye></FaBullseye> Number of recipes : {number_of_recipes}{" "}
+          recipes
+        </p>
+        <p className="flex items-center gap-2 text-base font-medium mb-5">
+          <FaBullseye></FaBullseye> Likes : {likes}
+        </p>
+        <button className="btn btn-outline btn-error text-lg">
+          View Recipes
+        </button>
       </div>
     </div>
   );
